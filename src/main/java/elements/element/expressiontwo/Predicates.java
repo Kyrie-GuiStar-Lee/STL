86829631853f934c72d9e6187eda7eb75d18ff39
nicylisa <2630962220@qq.com>
2020-10-18 22:09:26 +0800

    增加提示

--- src/main/java/elements/element/expressiontwo/Predicates.java
+++ src/main/java/elements/element/expressiontwo/Predicates.java
@@ -1,11 +1,17 @@
 package elements.element.expressiontwo;
 import elements.abstracts.Expression;
 import elements.abstracts.ExpressionTwo;
+import elements.element.RealNum;
 
 public class Predicates extends ExpressionTwo {
     public String sigName;
     public String type;
-    public Double num;
+    /**
+     * 2020-10-18
+     * tong
+     * 将数字类作为成员
+     */
+    public RealNum num;
     public Predicates(Expression left,Expression right){
         super(left,right);
     }
